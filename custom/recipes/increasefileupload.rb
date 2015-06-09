db1f6c53610af09a907738319e2a91b5ea0a4c6f
forrestmid <forrest@insightautomotive.com>
2015-06-08 18:02:39 -0600

    Update increasefileupload.rb

--- custom/recipes/increasefileupload.rb
+++ custom/recipes/increasefileupload.rb
@@ -1,17 +1,7 @@
-attribute "php/error_reporting",
-  :display_name => "Error reporting level",
-  :description => "Sets PHP Error Reporting in php.ini",
-  :default => "E_ALL & ~E_DEPRECATED"
-
 attribute "php/memory_limit",
   :display_name => "PHP Memory limit",
   :description => "Sets Memory Limit",
   :default => "512MB"
-
-attribute "php/error_log",
-  :display_name => "Error log path",
-  :description => "Sets path for Error log",
-  :default => "/var/log/php_error.log"
 
 attribute "php/post_max_size",
   :display_name => "Post Max Size",
@@ -23,17 +13,6 @@ attribute "php/upload_max_filesize",
   :description => "Sets Memory Limit for php",
   :default => "32M"
 
-attribute "php/allow_url_fopen",
-  :display_name => "Allow url fopen",
-  :description => "Sets Memory Limit for php",
-  :default => "Off"
-
-attribute "php/date_timezone",
-  :display_name => "Date timezone",
-  :description => "Sets the timezone",
-  :default => "Europe/London"
-          
-    
     
 %w{ubuntu debian}.each do |os|
   supports os
